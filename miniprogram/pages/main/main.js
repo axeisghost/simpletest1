62915c672fafb3670ced0ae9ab992a7da2f1352b
axeisghost <wjx941218@gmail.com>
2020-10-13 21:58:20 -0700

    axeisghost's test change

--- miniprogram/pages/main/main.js
+++ miniprogram/pages/main/main.js
@@ -7,9 +7,15 @@ Page({
     list:[]
   },
   onLoad: function (options) {
-    db.collection('emall').get({
+    db.collection('todos').count({
+      success: function(res) {
+        // res.data 包含该记录的数据
+        console.log(res.total)
+      }
+    })
+    db.collection('emall').where({_id: 'd782d4875f7c6ec200f2008613940691'}).get({
       success:res =>{
-        console.log(res.data)
+        console.log(res)
       }
     })
   },
@@ -20,6 +26,12 @@ Page({
 
   },
   onChange(event) {
+    db.collection('todos').count({
+      success: function(res) {
+        // res.data 包含该记录的数据
+        console.log(res.total)
+      }
+    })
     console.log(event);
     Notify({ type: 'primary', message: event.detail });
   },
